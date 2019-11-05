1fe05743c0355ad9efdc33ea6e3a42e50558bf85
Lucas Rosa <lucas.rosa2006@gmail.com>
2019-11-05 13:37:26 -0300

    Validações

--- src/main/resources/static/controllers/cliente.js
+++ src/main/resources/static/controllers/cliente.js
@@ -1,56 +1,57 @@
-app.controller("CadastroClienteCtrl", function ($scope, $http) {
-     
-	$scope.cliente = {}
-	$scope.clientes = []
-	$scope.clienteDetalhado = []
+app.controller("CadastroClienteCtrl", function($scope, $http) {
 
-	$('#sucesso').hide();
-	$('#erro').hide();
-	
-    $scope.cadastrar = function (){
-		$http({
-            method: "POST",  
-            url: "/cliente",  
-            datatype: "json",  
+    $scope.cliente = {}
+    $scope.clientes = []
+    $scope.clienteDetalhado = []
+
+    $('#sucesso').hide();
+    $('#erro').hide();
+
+    //Cadastrar o Cliente
+    $scope.cadastrar = function() {
+        $http({
+            method: "POST",
+            url: "/cliente",
+            datatype: "json",
             data: JSON.stringify($scope.cliente)
-    	  }).then(function mySuccess(response) {
-    	    $scope.clientes = response.data;
-    	    $('#sucesso').show()
-    	    window.location.href='../../partes/cliente/index.html'
-    	  }, function myError(response) {
-    	    $('#erro').show()
-    	    $scope.cliente = {}
-    	  });
+        }).then(function mySuccess(response) {
+            $scope.clientes = response.data;
+            $('#sucesso').show()
+            window.location.href = '../../partes/cliente/index.html'
+        }, function myError(response) {
+            $('#erro').show()
+            $scope.cliente = {}
+        });
     };
-    
-    function exibirPessoa (){
-    	$http({
-    		  method: 'GET',
-    		  url: '/cliente',
-    		}).then(function successCallback(response) {
-    			$scope.clientes = response.data
-    		  }, function errorCallback(response) {
-    		    // called asynchronously if an error occurs
-    		    // or server returns response with an error status.
-    		  });
-    }
-    
-    exibirPessoa();
-    
-    function detalhar(){
-    	$http({
-    		  method: 'GET',
-    		  url: '/cliente/' + 1,
-    		}).then(function successCallback(response) {
-    			$scope.clienteDetalhado = response.data
-    			console.log($scope.clienteDetalhado)
-    		  }, function errorCallback(response) {
-    		    // called asynchronously if an error occurs
-    		    // or server returns response with an error status.
-    		  });
-    }
-    
-    detalhar()
-    
-});
 
+    function exibirPessoa() {
+        $http({
+            method: 'GET',
+            url: '/cliente',
+        }).then(function successCallback(response) {
+            $scope.clientes = response.data
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+    }
+
+    exibirPessoa();
+
+
+    function detalhar() {
+        $http({
+            method: 'GET',
+            url: '/cliente/' + 1,
+        }).then(function successCallback(response) {
+            $scope.clienteDetalhado = response.data
+            console.log($scope.clienteDetalhado)
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+    }
+
+    detalhar()
+
+});
--- src/main/resources/static/controllers/prestador.js
+++ src/main/resources/static/controllers/prestador.js
@@ -1,56 +1,58 @@
-app.controller("CadastroPrestadorCtrl", function ($scope, $http) {
-     
-	$scope.prestador = {}
-	$scope.prestadores = []
-	$scope.prestadorDetalhado = []
-	
-	$('#sucesso').hide();
-	$('#erro').hide();
-	
-    $scope.cadastrar = function (){
-		$http({
-            method: "POST",  
-            url: "/prestador",  
-            datatype: "json",  
-            data: JSON.stringify($scope.prestador)
-    	  }).then(function mySuccess(response) {
-    	    $scope.prestadores = response.data;
-    	    $scope.prestador = {}
-    	    $('#sucesso').show()
-    	    window.location.href='../../partes/prestador/index.html'
-    	  }, function myError(response) {
-      	    $('#erro').show()
-      	    $scope.prestador = {}
-    	  });
-    };
-    
-    function exibir(){
-    	$http({
-    		  method: 'GET',
-    		  url: '/prestador',
-    		}).then(function successCallback(response) {
-    			$scope.prestadores = response.data
-    		  }, function errorCallback(response) {
-    		    // called asynchronously if an error occurs
-    		    // or server returns response with an error status.
-    		  });
-    }
-    
-    exibir();
-    
-    function detalhar(){
-    	$http({
-    		  method: 'GET',
-    		  url: '/prestador/' + 1,
-    		}).then(function successCallback(response) {
-    			$scope.prestadorDetalhado = response.data
-    		  }, function errorCallback(response) {
-    		    // called asynchronously if an error occurs
-    		    // or server returns response with an error status.
-    		  });
-    }
-    
-    detalhar()
-    
-});
+app.controller("CadastroPrestadorCtrl", function($scope, $http) {
 
+    $scope.prestador = {}
+    $scope.prestadores = []
+    $scope.prestadorDetalhado = []
+
+    $('#sucesso').hide();
+    $('#erro').hide();
+
+    //Cadastrar Prestador
+    $scope.cadastrar = function() {
+        console
+        /*
+		$http({
+            method: "POST",
+            url: "/prestador",
+            datatype: "json",
+            data: JSON.stringify($scope.prestador)
+        }).then(function mySuccess(response) {
+            $scope.prestadores = response.data;
+            $scope.prestador = {}
+            $('#sucesso').show()
+            window.location.href = '../../partes/prestador/index.html'
+        }, function myError(response) {
+            $('#erro').show()
+            $scope.prestador = {}
+        });*/
+    };
+
+    function exibir() {
+        $http({
+            method: 'GET',
+            url: '/prestador',
+        }).then(function successCallback(response) {
+            $scope.prestadores = response.data
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+    }
+
+    //exibir();
+
+    function detalhar() {
+        $http({
+            method: 'GET',
+            url: '/prestador/' + 1,
+        }).then(function successCallback(response) {
+            $scope.prestadorDetalhado = response.data
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+    }
+
+    //detalhar()
+
+});
