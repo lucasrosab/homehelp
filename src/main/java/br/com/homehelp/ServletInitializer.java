6ad3dc94ba36784b6a0ed87ae058cb64cc8ab87c
Lucas Rosa <lucas.rosa2006@gmail.com>
2019-11-30 11:24:03 -0300

    Correções Tela de Configuração e Solicitação Cli/Pres

--- src/main/java/br/com/homehelp/ServletInitializer.java
+++ src/main/java/br/com/homehelp/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(ApiwebApplication.class);
+		return application.sources(ApiwebApplication.class); 
 	} 
 
 }
